b4459562a614a45a05d74149e187c479ca263f4a
Oeny123 <johnjuni411@gmail.com>
2025-07-05 18:30:02 +0800

    initializing models

--- base/models.py
+++ base/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
 # Create your models here.
+class Room(models.Model):
+    # host =
+    # topic =
+    name = models.CharField(max_length=200)
+    # participants =
+    description = models.TextField(null=True, blank=True)
+    updated = models.DateTimeField(auto_now=True)
+    created = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return str(self.name)
